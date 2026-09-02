0bd1c99893bdec397cc51fa846d291413855224c
Lucas Marinoto <lucas_marinoto@msn.com>
2026-09-01 22:11:40 -0300

    test: add Spring Boot context test

---
+++ nebula-api/src/test/java/com/example/nebula/NebulaApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.nebula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
