8dc66d859ea807ca210ba4a510ed1b86a52a6e41
Lucas Marinoto <lucas_marinoto@msn.com>
2026-09-01 22:11:19 -0300

    feat: add sample Spring Boot entrypoint

---
+++ nebula-api/src/main/java/com/example/nebula/NebulaApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.nebula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NebulaApiApplication.class, args);
+    }
+}
